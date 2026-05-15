726d2b882c960f8aec9b9d5096e9db7f2b119b89
andamorim2206 <deds5879@gmail.com>
2026-05-15 13:46:36 -0300

    initial commit

---
+++ backend/src/app.module.js
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ClientsModule.register([
+      {
+        name: 'RABBITMQ_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.RABBITMQ_URL || 'amqp://admin:admin@localhost:5672'],
+          queue: 'pagamentos_queue',
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
+})
+export class AppModule {}
